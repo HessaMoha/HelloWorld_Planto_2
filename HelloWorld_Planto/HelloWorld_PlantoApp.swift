3e6ed4818b364502589a21773d2ee91bfa0cfa1b
HessaMoha <hessaalajaleen@gmail.com>
2024-10-20 14:28:10 +0300

    Initial Commit

---
+++ HelloWorld_Planto/HelloWorld_PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld_PlantoApp.swift
+//  HelloWorld_Planto
+//
+//  Created by حصه العجالين on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld_PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
