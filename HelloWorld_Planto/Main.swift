9814389af6c7dfdd7236ce8a40853bca37583f40
HessaMoha <hessaalajaleen@gmail.com>
2024-10-21 14:50:31 +0300

    Splash Screen

---
+++ HelloWorld_Planto/Main.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  HelloWorld_Planto
+//
+//  Created by حصه العجالين on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Main()
+}
---
+++ HelloWorld_Planto/Splash.swift
@@ -0,0 +1,18 @@
+//
+//  Splash.swift
+//  HelloWorld_Planto
+//
+//  Created by حصه العجالين on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+struct Splash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Splash()
+}
